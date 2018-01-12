4c3ed8c4584bf243b444647bbfd834dc589776fd
Christopher Jerome Herrera <Christopherjeromeherrera@gmail.com>
2018-01-11 21:00:07 -0700

    first push

---
+++ webpage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<link rel="stylesheet" type='text/css' href='layout.css'>
+<p style="text-align:center;"><img src="Chris.jpg" alt="Chris"></p>
+<header>
+    <h1 align="center">Chris J. Herrera</h1>
+    <h2 align="center"> About me</h2>
+</header>
+<section>
+    <ul>
+        <li><em>Professional</em><br>Jr. Web developer, Certified AT&T classroom instructor, passionate about continued professional growth.</li>
+        <li><em>Personal</em><br>Father of four, happily married, avid motorcycle rider.</li>
+    </ul>
+</section>
+<div>
+    <p align="center">I am deeply passionate about web development and design. Constantly learning and improving to provide the highest quality product.</p>
+</div>
+
+<img src="Git%20Fire.jpg">
+
+<iframe width="554" height="360" src="https://www.youtube.com/embed/kVPN0w1BWQw" frameborder="0"
+        allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+<p>Click below for a good laugh!!!</p>
+<a href="https://www.youtube.com/watch?v=wXQUhX89vtQ">Meow</a>
+
+<div>
+    <audio controls>
+        Your browser does not support the <code>audio</code> element.
+        <source src="Star Trek TOS  Original theme.mp3">
+    </audio>
+</div>
+
+<footer>
+    Built by CJH Inc.
+</footer>
+
+</body>
+</html>
